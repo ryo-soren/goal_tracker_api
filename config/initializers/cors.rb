f017ad05669856c6b106b689a1d4d7a5a14241eb
ryoS <ryosorensen0606@gmail.com>
2024-01-26 22:25:34 -0800

    updated cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins '127.0.0.1:5500', 'localhost:5500'
+      origins '127.0.0.1:5500', 'localhost:5500', 'https://goal-tracker-izm1.onrender.com'
   
       resource(
         '/api/*',
