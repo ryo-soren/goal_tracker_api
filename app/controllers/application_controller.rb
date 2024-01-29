d3e93a2c256e5d4408fcab96d97570d624e56917
ryoS <ryosorensen0606@gmail.com>
2024-01-29 15:25:01 -0800

    Added check for session[:user_id] to current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
 
     def current_user
+        puts "$$$$$$"
+        puts session[:user_id]
+        puts "$$$$$$"
         return unless session[:user_id]
         @current_user ||= User.find_by_id(session[:user_id])
     end
