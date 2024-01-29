1ed8ab0be68ef3372e42cb17b415198d3c56c43d
ryoS <ryosorensen0606@gmail.com>
2024-01-28 16:43:23 -0800

    Changed current_user and moved to base application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
 
-    # def current_user
-    #     return unless session[:user_id]
-    #     @current_user ||= User.find_by_id(session[:user_id])
-    # end
+    def current_user
+        return unless session[:user_id]
+        @current_user ||= User.find_by_id(session[:user_id])
+    end
     
     # def current_user
     #     @current_user ||= User.find_by_id session[:user_id]
     # end
 
-    # helper_method :current_user
+    helper_method :current_user
 
 end
