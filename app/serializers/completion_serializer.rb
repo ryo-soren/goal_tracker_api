8f9ede2a469f9e1b274a3ebeb1cdb79773f78b8e
ryoS <ryosorensen0606@gmail.com>
2024-02-05 15:02:18 -0800

    Updated serialiers

--- app/serializers/completion_serializer.rb
+++ app/serializers/completion_serializer.rb
@@ -1,7 +1,4 @@
 class CompletionSerializer < ActiveModel::Serializer
-  belongs_to :user
-  belongs_to :goal
-
   attributes :id, :goal_id, :frequency, :created_at
 
   def frequency
--- app/serializers/goal_serializer.rb
+++ app/serializers/goal_serializer.rb
@@ -1,11 +1,6 @@
 class GoalSerializer < ActiveModel::Serializer
-  # belongs_to :user
+  belongs_to :user
   has_many :completions
 
-  attributes :id, :title, :description, :frequency, :times, :done, :successful, :unsuccessful, :deadline, :created_at, :created_by, :completions
-
-  def created_by
-    object.user&.username
-  end
-  
+  attributes :id, :title, :description, :frequency, :times, :done, :successful, :unsuccessful, :deadline, :created_at, :completions, :user
 end
